9092c637bed0f528444030c02bdaf9a4a3973f04
Chloe Chen <chloechen727@gmail.com>
2014-12-20 17:02:46 +0800

    Add walking_stairs using the factorial way

---
+++ walk_stairs/walking_stairs_with_factorial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-  
+import os, sys
+
+FIVE_STEPS = 5
+THREE_STEPS = 3
+ONE_STEPS = 1
+
+def total_combination(n):
+	total_combination_result = 0
+
+	for element_z in range(0, n//FIVE_STEPS+1, +1):
+		tmp_value_without_each_fixed_z = n - FIVE_STEPS*element_z
+
+		for element_y in range(0, tmp_value_without_each_fixed_z//THREE_STEPS+1, +1):
+			element_x = tmp_value_without_each_fixed_z - THREE_STEPS*element_y
+			
+			this_conmbination = combination(element_x, element_y, element_z)
+			total_combination_result = total_combination_result + this_conmbination
+
+	return total_combination_result
+
+def combination(x, y, z):
+	return (factorial(x+y+z)/(factorial(x)*factorial(y)*factorial(z)))
+
+def factorial(number):
+	total = 1
+	for element in range(number, 1, -1):
+		total = total * element
+	return total
+
+n = int(sys.argv[1])
+
+print ('Total combination of ' + str(n) + ' stairs : ' + str(total_combination(n)))
