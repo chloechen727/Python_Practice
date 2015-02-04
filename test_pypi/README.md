5f9a881c5bc2846df3fe8cdd1945b0cd274c495e
ChloeChen <chloechen727@gmail.com>
2015-02-04 16:26:38 +0800

    Add test_pypi to practice upload package to PyPI

---
+++ test_pypi/README.md
@@ -0,0 +1,29 @@
+***
+=====
+
+
+Installation
+------------
+
+If you have Python with Pip or EasyInstall installed, simply run:
+
+```shell
+pip install ***
+```
+
+or
+
+```shell
+easy_install install ***
+```
+
+Alternatively, if you perfer to install it from source, just clone the repository and run:
+
+```shell
+python setup install
+```
+
+License
+-------
+
+MIT
---
+++ test_pypi/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  name = '***',
+  license = 'MIT',
+  packages = [
+      'test_pypi',
+  ],
+  version = '0.1',
+  description = 'test_pypi is a package, just for practice upload package to ',
+  author = 'Chloe Chen',
+  author_email = 'chloechen727@gmail.com',
+  url = 'https://github.com/chloechen727/Python_Practice/test_pypi',
+  keywords = [
+      'test_pypi',
+  ],
+  classifiers = [],
+)
